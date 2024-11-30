ffe949c4d4ed1b87fec17cf498cf6d0a06c95fb5
प्रेम बाबू <144128294+prem-project3608@users.noreply.github.com>
2024-11-30 12:51:39 +0530

    Delete install-yt-dlp.sh

--- install-yt-dlp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# Create local bin directory
-mkdir -p ./bin
-
-# Download yt-dlp to the local bin directory
-curl -L https://github.com/yt-dlp/yt-dlp/releases/download/2024.09.13/yt-dlp_linux_amd64 -o ./bin/yt-dlp
-chmod a+rx ./bin/yt-dlp
